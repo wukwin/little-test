5430d3372fd40ecc397336a4c79b7879b901bf0f
wukwin <812185183@qq.com>
2016-04-11 11:08:55 +0800

    index.php
    don't worry ,just a test.

---
+++ index.php
@@ -0,0 +1,39 @@
+-<!DOCTYPE html>
+-<html>
+-<head>
+-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+-<title>little test</title>
+-<style>
+-    .hide{display:none; background:red;}
+-    a{text-decoration:none;}
+<script src="http://apps.bdimg.com/libs/jquery/1.11.1/jquery.js"></script>
+-</style>
+-</head>
+-<body>
+-    <ul>
+-        <li>1</li>
+-        <li>2</li>
+-        <li>3</li>
+-        <li>4</li>
+-        <a href="#">more</a>
+-        <li class="hide">5</li>
+-        <li class="hide">6</li>
+-        <li class="hide">7</li>
+-        <li>8</li>
+-    </ul>
+-</body>
+-<script type="text/javascript">
+-   $(function(){
+-      $("a").click(function(){
+-          if($("a").html()=="more"){
+-              $(".hide").show();
+-              $("a").html("simple");
+-          }
+-          else{
+-              $(".hide").hide();
+-              $("a").html("more");
+-          }
+-      });  
+-})
+-</script>
+-</html>
