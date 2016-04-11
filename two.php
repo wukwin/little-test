e58db1917579f617e6ab0c15c48b4ccfa834245f
wukwin <812185183@qq.com>
2016-04-11 13:00:37 +0800

    yes
    what?

--- two.php
+++ two.php
@@ -1,5 +1,14 @@
-<?php  
-
-echo "what?";
-
-?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<title>upload</title>
+</head>
+<body>
+	<form action="doAction.php" method="post">
+	please choose your file:
+	<input type="file" name="myfile" />
+	<input type="submit" value="upload your file">
+	</form>
+</body>
+</html>
