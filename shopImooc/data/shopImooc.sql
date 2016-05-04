ddc07a66fa99c72e1cbc4432b4db1e807032d6ab
wukwin <812185183@qq.com>
2016-05-04 11:16:30 +0800

    yes

---
+++ shopImooc/data/shopImooc.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE IF NOT EXISTS `shopImooc`;
+USE `shopImooc`;
+--管理员表
+DROP TABLE IF EXISTS `imooc_admin`;
+CREATE TABLE `imooc_admin`(
+`id` tinyint unsigned auto_increment key,
+`username` varchar(20) not null unique,
+`password` varchar(32) not null,
+`email` varchar(50) not null
+);
+
+--分类表
+DROP TABLE IF EXISTS `imooc_cate`;
+CREATE TABLE `imooc_cate`(
+`id` smallint unsigned auto_increment key,
+`cName` varchar(50) unique
+);
+
+--商品表
+DROP TABLE IF EXISTS `imooc_pro`;
+CREATE TABLE `imooc_pro`(
+`id` int unsigned auto_increment key,
+`pName` varchar(50) not null unique,
+`pSn` varchar(50) not null,
+`pNum` int unsigned DEFAULT 1,
+`mprice` decimal(10,2) not null,
+`iPrice` decimal(10,2) not null,
+`pDesc` text,
+`pImg` varchar(50) not null,
+`pubTime` int unsigned not null,
+`isShow` tinyint(1) DEFAULT 1,
+`isHot` tinyint(1) DEFAULT 0,
+`cId` smallint unsigned not null
+);
+
+--用户表
+DROP TABLE IF EXISTS `imooc_user`;
+CREATE TABLE `imooc_user`(
+`id` int unsigned auto_increment key,
+`username` varchar(20) not null unique,
+`password` varchar(32) not null,
+`sex` enum("男","女","保密") not null DEFAULT "保密",
+`face` varchar(50) not null,
+`regTime` int unsigned not null
+
+);
+--相册表
+DROP TABLE IF EXISTS `imooc_album`;
+CREATE TABLE `imooc_album`(
+`id` int unsigned auto_increment key,
+`pId` int unsigned not null,
+`albumPath` varchar(50) not null
+);
