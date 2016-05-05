3e7cc76f5ecb0661091b4cf2c996251bea0db46f
wukwin <812185183@qq.com>
2016-05-05 15:42:39 +0800

    delete

--- shopImooc/admin/doLogin.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$password=$_POST['password'];
-$username=$_POST['username'];
-$verify=$_POST['verify'];
-$verify1=$_SESSION['verify'];
-
--- shopImooc/lib/mysql.func.php
+++
@@ -1,6 +0,0 @@
-<?php 
-function connect(){
-
-	mysql_connect("localhost","root","root") or die("数据库连接失败Error".mysql_errno().":".mysql_error());
-	
-}
